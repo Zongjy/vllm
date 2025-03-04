84a98627c2632cb12f4ca136f7ac6970d4bdb48f
Zongjy <zongjy.777@gmail.com>
2025-03-04 10:51:08 +0000

    modify log to vllm.logger

--- alayadb/basic.py
+++ alayadb/basic.py
@@ -1,30 +1,15 @@
 # SPDX-License-Identifier: Apache-2.0
 
-import logging
 from vllm import LLM, SamplingParams
-from rich.console import Console
-from rich.table import Table
-from datetime import datetime
+import vllm.logger
 import os
 
 os.environ["VLLM_USE_V1"] = "1"
 # os.environ["VLLM_USE_SPARSE_OFFLOAD"] = 1
 os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
-
 os.makedirs("logs", exist_ok=True)
-log_filename = datetime.now().strftime("logs/vllm_%Y%m%d_%H%M%S.log")
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=[
-        logging.FileHandler(log_filename, encoding="utf-8"),
-        logging.StreamHandler(),
-    ],
-)
-logger = logging.getLogger("vllm_server")
-
-console = Console()
+logger = vllm.logger.logger
 
 # Sample prompts.
 prompts = [
@@ -38,25 +23,16 @@ sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 # Create an LLM.
 model = "meta-llama/Llama-3.2-1B-Instruct"
-console.print("[bold magenta] Loading model... [/bold magenta]")
-logger.info(f"Loading model: {model}")
 llm = LLM(model=model, enforce_eager=True, gpu_memory_utilization=0.7)
-logger.info("Loading model successful.")
 
 # Generate texts from the prompts. The output is a list of RequestOutput objects
 # that contain the prompt, generated text, and other information.
 outputs = llm.generate(prompts, sampling_params)
 
-table = Table(title="vLLM generated texts", show_lines=True)
-table.add_column("Prompt", justify="left", style="cyan", no_wrap=False)
-table.add_column("Generated Text", justify="left", style="green")
-
 # Print the outputs.
 for output in outputs:
     prompt = output.prompt
     generated_text = output.outputs[0].text.strip()
-    table.add_row(prompt, generated_text)
-    logger.info(f"Prompt: {prompt} -> Generated: {generated_text}")
+    logger.info_once(f"Prompt: {prompt} -> Generated: {generated_text}")
 
-console.print(table)
-logger.info("Finished generating texts.")
+logger.info_once("Finished generating texts.")
--- alayadb/openai_client.py
+++ alayadb/openai_client.py
@@ -1,25 +1,10 @@
-import logging
 from openai import OpenAI
-from rich.console import Console
-from datetime import datetime
+import vllm.logger
 import os
 
 os.makedirs("logs", exist_ok=True)
-log_filename = datetime.now().strftime("logs/openai_client_%Y%m%d_%H%M%S.log")
 
-# logging configuration
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=[
-        logging.FileHandler(log_filename, encoding="utf-8"),
-        logging.StreamHandler(),
-    ],
-)
-logger = logging.getLogger("openai_client")
-
-# Rich Console
-console = Console()
+logger = vllm.logger.logger
 
 # Set API Key & Base URL
 openai_api_key = "EMPTY"
@@ -30,8 +15,7 @@ client = OpenAI(
     base_url=openai_api_base,
 )
 
-console.print("[bold yellow] Sending Request to vLLM Server...[/bold yellow]")
-logger.info("Sending Request to vLLM Server.")
+logger.info_once("Sending Request to vLLM Server.")
 
 model = "meta-llama/Llama-3.2-1B-Instruct"
 try:
@@ -43,9 +27,7 @@ try:
     )
     response_text = chat_response.choices[0].message.content
 
-    console.print(f"[bold green] Server Response:[/bold green] {response_text}")
-    logger.info(f"Server Response: {response_text}")
+    logger.info_once(f"Server Response: {response_text}")
 
 except Exception as e:
     logger.error(f"Request failed: {str(e)}")
-    console.print(f"[bold red] Request failed: {str(e)}[/bold red]")
